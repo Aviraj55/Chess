ff72e6608c0aea5485e13faad2040594e55237cf
Caleb Hock <crhy9x@mst.edu>
2020-10-12 23:08:29 -0500

    Fix board population, reformat code

--- src/game/board.cpp
+++ src/game/board.cpp
@@ -1,6 +1,12 @@
 #include "board.h"
 
+#include "piece/bishop.h"
+#include "piece/king.h"
+#include "piece/knight.h"
+#include "piece/pawn.h"
 #include "piece/piece.h"
+#include "piece/queen.h"
+#include "piece/rook.h"
 
 Board::Board() {
   board = new Piece **[HEIGHT];
@@ -14,36 +20,48 @@ Board::Board() {
   // Populate the board with pieces
   for (int file = 0; file < WIDTH; ++file) {
     // Insert white pawn
-    board[1][file] = new Pawn(board, Piece::Color::WHITE, Coordinate(1, file));
+    board[1][file] = new Pawn(this, Piece::Color::WHITE, Coordinate(1, file));
     // Insert black pawn
-    board[HEIGHT - 2][file] = new Pawn(board, Piece::Color::BLACK, Coordinate(HEIGHT - 2, file));
+    board[HEIGHT - 2][file] =
+        new Pawn(this, Piece::Color::BLACK, Coordinate(HEIGHT - 2, file));
   }
 
   // Insert rooks
-  board[0][0] = new Rook(board, Piece::Color::WHITE, Coordinate(0, 0));
-  board[0][WIDTH - 1] = new Rook(board, Piece::Color::WHITE, Coordinate(0, WIDTH - 1));
-  baord[HEIGHT - 1][0] = new Rook(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 0));
-  board[HEIGHT - 1][WIDTH - 1] = new Rook(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, WIDTH - 1));
+  board[0][0] = new Rook(this, Piece::Color::WHITE, Coordinate(0, 0));
+  board[0][WIDTH - 1] =
+      new Rook(this, Piece::Color::WHITE, Coordinate(0, WIDTH - 1));
+  board[HEIGHT - 1][0] =
+      new Rook(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 0));
+  board[HEIGHT - 1][WIDTH - 1] =
+      new Rook(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, WIDTH - 1));
 
   // Insert knights
-  board[0][1] = new Knight(board, Piece::Color::WHITE, Coordinate(0, 1));
-  board[0][WIDTH - 2] = new Knight(board, Piece::Color::WHITE, Coordinate(0, WIDTH - 2));
-  baord[HEIGHT - 1][1] = new Knight(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 1));
-  board[HEIGHT - 1][WIDTH - 2] = new Knight(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, WIDTH - 2));
+  board[0][1] = new Knight(this, Piece::Color::WHITE, Coordinate(0, 1));
+  board[0][WIDTH - 2] =
+      new Knight(this, Piece::Color::WHITE, Coordinate(0, WIDTH - 2));
+  board[HEIGHT - 1][1] =
+      new Knight(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 1));
+  board[HEIGHT - 1][WIDTH - 2] =
+      new Knight(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, WIDTH - 2));
 
   // Insert bishops
-  board[0][2] = new Bishop(board, Piece::Color::WHITE, Coordinate(0, 2));
-  board[0][WIDTH - 3] = new Bishop(board, Piece::Color::WHITE, Coordinate(0, WIDTH - 3));
-  baord[HEIGHT - 1][2] = new Bishop(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 2));
-  board[HEIGHT - 1][WIDTH - 3] = new Bishop(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, WIDTH - 3));
+  board[0][2] = new Bishop(this, Piece::Color::WHITE, Coordinate(0, 2));
+  board[0][WIDTH - 3] =
+      new Bishop(this, Piece::Color::WHITE, Coordinate(0, WIDTH - 3));
+  board[HEIGHT - 1][2] =
+      new Bishop(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 2));
+  board[HEIGHT - 1][WIDTH - 3] =
+      new Bishop(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, WIDTH - 3));
 
   // Insert queens
-  board[0][3] = new Queen(board, Piece::Color::WHITE, Coordinate(0, 3));
-  baord[HEIGHT - 1][3] = new Queen(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 3));
-  
+  board[0][3] = new Queen(this, Piece::Color::WHITE, Coordinate(0, 3));
+  board[HEIGHT - 1][3] =
+      new Queen(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 3));
+
   // Insert kings
-  board[0][4] = new King(board, Piece::Color::WHITE, Coordinate(0, 4));
-  baord[HEIGHT - 1][4] = new King(board, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 4));
+  board[0][4] = new King(this, Piece::Color::WHITE, Coordinate(0, 4));
+  board[HEIGHT - 1][4] =
+      new King(this, Piece::Color::BLACK, Coordinate(HEIGHT - 1, 4));
 }
 
 Piece *Board::piece_at(const Coordinate &coordinate) {
